ef56d5db166ddb8260e30ec173c4d9aa5e8d054f
Shamil Qurbonov <qurbonovshamil@gmail.com>
2021-03-15 15:56:38 +0700

    Initi app

---
+++ main.ts
@@ -0,0 +1,8 @@
+let nameUs: string = 'Shamil Qurbonov'
+const age: number = 123
+const isPresent: boolean = true
+
+
+console.log('First and last name: ', nameUs)
+console.log(age)
+console.log('isPresent', isPresent)
